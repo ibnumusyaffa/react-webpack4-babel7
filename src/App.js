3bdcbea33508c7eb7688c9f944e690a5c65f3d98
ibnu musyaffa <ibnu.musyaffa@gmail.com>
2018-10-04 09:22:24 +0700

    add styled component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
+import styled from 'styled-components';
 
+const Container = styled.div`
+  color: red;
+`;
 class App extends Component {
   render() {
-    return <div>hello</div>;
+    return <Container>App component</Container>;
   }
 }
 
